8e73e0173b69718f9ac09166a328500466f79688
Maurício Vieira <mauricio@mauriciovieira.net>
2011-09-28 18:06:10 -0300

    3

--- placarTenisTest.py
+++ placarTenisTest.py
@@ -8,7 +8,12 @@ class PlacarTenisTest(unittest.TestCase):
    
     def testePlacar0x0(self):
         self.assertEquals(self.placar.getplacar(),"0x0")
+    def testeSeExistem2Jogadores(self):
+        self.assertEquals(self.placar.jogador1,"jogad1")
+        self.assertEquals(self.placar.jogador2,"jogad2")
         
+    def testeSejo
+            
     
 if __name__ == '__main__':
     unittest.main()
