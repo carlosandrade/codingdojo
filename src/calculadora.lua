8b8b62e49d0717d5f43b4e05df22554870e708aa
Maurício Vieira <mauricio@mauriciovieira.net>
2011-11-07 18:28:28 -0200

    6

--- src/calculadora.lua
+++ src/calculadora.lua
@@ -11,8 +11,21 @@ function soma_romana(num1,num2)
     return t
   end
 
+    local num1separado = separa_string (num1)
+    local valornum1 = 0
     
-  local soma = valores[num1] + valores[num2]
+    for n = 1, #num1separado do
+        valornum1 = valores[num1separado[n]]
+    end
+    
+        local num2separado = separa_string (num2)
+    local valornum2 = 0
+    
+    for n = 1, #num2separado do
+        valornum2 = valores[num2separado[n]]
+    end
+    
+  local soma = valornum1
   
   
   return soma
