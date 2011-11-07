c3264f2d9735ccd347e33529ac452930b7e41a42
Maurício Vieira <mauricio@mauriciovieira.net>
2011-11-07 18:10:41 -0200

    3

--- src/calculadora.lua
+++ src/calculadora.lua
@@ -1,6 +1,8 @@
 function soma_romana(num1,num2) 
     
-  local valores = { I = 1, V = 5, X = 10 }
+  local valores = { I = 1, V = 5, X = 10, L = 50, C = 100, D = 500 }
   local soma = valores[num1] + valores[num2]
+  
+  
   return soma
 end
