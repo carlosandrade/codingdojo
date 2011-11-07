25b203d7bf7e2ef6389bdf2949af41dd0ef30e2c
Maurício Vieira <mauricio@mauriciovieira.net>
2011-11-07 18:04:44 -0200

    2

--- specs/calculadora_spec.lua
+++ specs/calculadora_spec.lua
@@ -5,8 +5,14 @@ describe["Calculadora Romana"] = function()
   it["I + I deve ser II"] = function()
   	expect( soma_romana( "I", "I") ).should_be(2)
   end
-  it["I + II deve ser III"] = function()
-    expect( soma_romana( "I", "II")).should_be(3)
+  it["V + V deve ser X"] = function()
+    expect( soma_romana( "V", "V")).should_be(10)
+  end
+  it["X + X deve ser XX"] = function()
+    expect( soma_romana( "X", "X")).should_be(20)
+  end
+  it["X + XX deve ser XXX"] = function()
+    expect( soma_romana( "X", "XX")).should_be(30)
   end
 end
 
--- src/calculadora.lua
+++ src/calculadora.lua
@@ -1,13 +1,6 @@
 function soma_romana(num1,num2) 
-  
-  local soma = 0
-  
-  local valores = { I = 1}
-  soma = 
-                   
---  if (valores[num1] and valores[num2]) then
---    soma = valores[num1] + valores[num2]
---  end
- 
+    
+  local valores = { I = 1, V = 5, X = 10 }
+  local soma = valores[num1] + valores[num2]
   return soma
 end
